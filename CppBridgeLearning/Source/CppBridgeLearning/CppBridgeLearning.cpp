09477d283ced8749ab9dcafd0fb65e105805511c
wawaw <wawawa0000000gmail.com.com>
2026-06-21 02:33:54 +0900

    StartupModuleにUE_LOGとAddOnScreenDebugMessageの動作確認コードを追加

--- CppBridgeLearning/Source/CppBridgeLearning/CppBridgeLearning.cpp
+++ CppBridgeLearning/Source/CppBridgeLearning/CppBridgeLearning.cpp
@@ -1,6 +1,19 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
-
 #include "CppBridgeLearning.h"
 #include "Modules/ModuleManager.h"
 
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, CppBridgeLearning, "CppBridgeLearning" );
+class FCppBridgeLearningGameModule : public FDefaultGameModuleImpl
+{
+public:
+	virtual void StartupModule() override
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Hello from CppBridgeLearning!"));
+
+		if (GEngine)
+		{
+			GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Yellow, TEXT("Hello from CppBridgeLearning"));
+		}
+	}
+};
+
+IMPLEMENT_PRIMARY_GAME_MODULE(FCppBridgeLearningGameModule, CppBridgeLearning, "CppBridgeLearning");
