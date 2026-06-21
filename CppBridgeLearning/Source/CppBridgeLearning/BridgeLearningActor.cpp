ddd74dd56d98478047eba68a6256bae3cbe73847
wawaw <wawawa0000000gmail.com.com>
2026-06-21 14:10:19 +0900

    BridgeLearningActorクラスを追加し、UPROPERTYによるBlueprint連携を実装

---
+++ CppBridgeLearning/Source/CppBridgeLearning/BridgeLearningActor.cpp
@@ -0,0 +1,30 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "BridgeLearningActor.h"
+
+// Sets default values
+ABridgeLearningActor::ABridgeLearningActor()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ABridgeLearningActor::BeginPlay()
+{
+	Super::BeginPlay();
+
+	if (GEngine)
+	{
+		FString Message = FString::Printf(TEXT("TestNumber = %d"), TestNumber);
+		GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Yellow, Message);
+	}
+}
+
+// Called every frame
+void ABridgeLearningActor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
---
+++ CppBridgeLearning/Source/CppBridgeLearning/BridgeLearningActor.h
@@ -0,0 +1,29 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "BridgeLearningActor.generated.h"
+
+UCLASS()
+class CPPBRIDGELEARNING_API ABridgeLearningActor : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ABridgeLearningActor();
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "BridgeLearning")
+	int32 TestNumber;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+};
